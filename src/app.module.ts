52ad606fd477d934c0bb1a7e29c2cc2fd19c8c93
Muhammad Habil <habilmuhammad20@gmail.com>
2022-12-13 09:49:35 +0700

    add .env.example and ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,18 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { PokemonsModule } from './pokemons/pokemons.module';
 
 @Module({
   imports: [
-    MongooseModule.forRoot(
-      'mongodb+srv://mhhabil:habil123@cluster0.6lqkbzi.mongodb.net/?retryWrites=true&w=majority',
-    ),
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.MONGO_DB_URL),
     UsersModule,
     AuthModule,
     PokemonsModule,
